60637acf29cab3fb0094a0dd92986e2d2be8d2bb
Peter Zemturis <pzemturis@gmail.com>
2024-04-23 16:11:49 +0200

    added getMovies

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -1,48 +1,56 @@
 const { connectionMySQL, queryDatabase } = require("../connectionMySQL");
 
 exports.getCategories = async (req, res) => {
-  try {
-    const getCategories = await queryDatabase(
-      "SELECT * FROM category WHERE categoryId = ?",
-      [10],
-    );
-    console.log(getCategories);
+	try {
+		const getCategories = await queryDatabase(
+			"SELECT * FROM category WHERE categoryId = ?",
+			[10]
+		);
+		console.log(getCategories);
 
-    if (getCategories.length > 0) {
-      res.json(getCategories);
-    } else {
-      res.json({
-        error: "Inget resultat",
-      });
-    }
-  } catch (error) {
-    return res.status(500).json({
-      error: error.message,
-    });
-  }
+		if (getCategories.length > 0) {
+			res.json(getCategories);
+		} else {
+			res.json({
+				error: "Inget resultat",
+			});
+		}
+	} catch (error) {
+		return res.status(500).json({
+			error: error.message,
+		});
+	}
 };
 
 exports.getCategory = async (req, res) => {
-  const { id } = req.params;
-  try {
-    let sql = 'SELECT categoryName FROM category WHERE title REGEXP "id?"';
+	const { id } = req.params;
+	try {
+		let sql = 'SELECT categoryName FROM category WHERE title REGEXP "id?"';
 
-    const getCategory = await queryDatabase(sql, [10]);
+		const getCategory = await queryDatabase(sql, [10]);
 
-    if (getCategory.length > 0) {
-      res.json(this.getCategory);
-    } else {
-      res.json({
-        error: "Inget resultat",
-      });
-    }
-  } catch (error) {
-    return res.status(500).json({
-      error: error.message,
-    });
-  }
+		if (getCategory.length > 0) {
+			res.json(this.getCategory);
+		} else {
+			res.json({
+				error: "Inget resultat",
+			});
+		}
+	} catch (error) {
+		return res.status(500).json({
+			error: error.message,
+		});
+	}
 };
 // movies get
 exports.getMovies = async (req, res) => {
-  res.send("hej");
+	// res.send("hej");
+	try {
+		const getMoviesArray = await queryDatabase("SELECT * From movie");
+		res.json(getMoviesArray);
+	} catch (e) {
+		return res.status(500).json({
+			error: e.message,
+		});
+	}
 };
