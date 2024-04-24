eef93657cc85a95549aa80b2a8b13d45c88e2813
Cecilia Rissanen <cecilia.rissanen@iths.com>
2024-04-24 15:46:55 +0200

    24/4

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -7,7 +7,7 @@ const {
 // get all categories
 exports.getCategories = async (req, res) => {
   try {
-    const getCategories = await queryDatabase("SELECT * category");
+    const getCategories = await queryDatabase("SELECT * FROM category");
     console.log(getCategories);
 
     if (getCategories.length > 0) {
@@ -27,7 +27,8 @@ exports.getCategories = async (req, res) => {
 // get category by Id
 exports.getCategory = async (req, res) => {
   const { id } = req.params;
-  let sql = "SELECT categoryName FROM category WHERE categoryId = ?";
+  let sql =
+    "SELECT categoryName FROM category WHERE categoryName LIKE CONCAT('%',?,'%')";
   try {
     const getCategory = await queryDatabase(sql, id);
 
@@ -45,14 +46,41 @@ exports.getCategory = async (req, res) => {
   }
 };
 
+// get category by search
 exports.getCategoryBySearch = async (req, res) => {
   const { search } = req.params;
-  let sql = "SELECT categoryName FROM category WHERE categoryName LIKE ?";
+  let sql =
+    "SELECT categoryName FROM category WHERE categoryName LIKE CONCAT('%',?,'%')";
   try {
     const getCategoryBySearch = await queryDatabase(sql, search);
-    console.log("Hej");
+
     if (getCategoryBySearch.length > 0) {
       res.json(getCategoryBySearch);
+    } else {
+      res.json({
+        error: "Inget resultat",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+// get movie by search with limit
+exports.getMovieBySearch = async (req, res) => {
+  const { keyword, amount } = req.params;
+  let sql =
+    "SELECT movieId, movieName, movieYear, moviePoster FROM movie WHERE movieName LIKE CONCAT('%',?,'%') LIMIT ?";
+  try {
+    const getMovieBySearch = await queryDatabase(sql, [
+      keyword,
+      Number(amount),
+    ]);
+
+    if (getMovieBySearch.length > 0) {
+      res.json(getMovieBySearch);
     } else {
       res.json({
         error: "Inget resultat",
@@ -84,10 +112,41 @@ exports.getMovies = async (req, res) => {
 exports.getActorsMovie = async (req, res) => {
   const { movie } = req.params;
   let sql =
-    "SELECT personName, personBorn FROM movie INNER JOIN movieJobPerson mJP on movie.movieId = mJP.movieJobPersonMID INNER JOIN job j on mJP.movieJobPersonJID = j.jobId INNER JOIN person p on mJP.movieJobPersonPID = p.personId WHERE movieName = ? AND jobTitle = 'Actor' ";
+    "SELECT personName, personBorn FROM movie INNER JOIN movieJobPerson mJP on movie.movieId = mJP.movieJobPersonMID INNER JOIN job j on mJP.movieJobPersonJID = j.jobId INNER JOIN person p on mJP.movieJobPersonPID = p.personId WHERE movieName LIKE CONCAT('%',?,'%') AND jobTitle = 'Actor'  ";
   try {
     const getActorArray = await queryDatabase(sql, movie);
     res.json(getActorArray);
+  } catch (e) {
+    return res.status(500).json({
+      error: e.message,
+    });
+  }
+};
+
+// get all movies from specific actor:
+
+exports.getActor = async (req, res) => {
+  const { actor } = req.params;
+  let sql =
+    "SELECT movieName, movieYear, movieId FROM movie INNER JOIN movieJobPerson mJP on movie.movieId = mJP.movieJobPersonMID JOIN job j on mJP.movieJobPersonJID = j.jobId JOIN person p on mJP.movieJobPersonPID = p.personId WHERE personName LIKE CONCAT('%',?,'%') ";
+  try {
+    const getActorArray = await queryDatabase(sql, actor);
+    res.json(getActorArray);
+  } catch (e) {
+    return res.status(500).json({
+      error: e.message,
+    });
+  }
+};
+
+// search db after person, category and persons obs: only works if procedure "searcher" exists
+exports.getFromSearch = async (req, res) => {
+  const { keyword } = req.params;
+  console.log("keyWord", keyword);
+  let sql = "call searcher( ? )";
+  try {
+    const getSearchResult = await queryDatabase(sql, keyword);
+    res.json(getSearchResult);
   } catch (e) {
     return res.status(500).json({
       error: e.message,
@@ -108,27 +167,145 @@ exports.importMovie = async (req, res) => {
 
       if (getMovieResults.success !== false) {
         const insertMovie_sql =
-          "INSERT INTO movie (movieName, movieYear, movieDescription, moviePoster, movieRuntime) VALUES (?, ?, ?, ?, ?)";
+          "INSERT INTO movie (movieName, movieYear, movieDescription, moviePoster, movieRuntime, movietmdbId) VALUES (?, ?, ?, ?, ?, ?)";
         const convertedYear = new Date(
           getMovieResults.release_date,
         ).getFullYear();
-        const moviePoster = `https://image.tmdb.org/t/p/w300${getMovieResults.poster_path}`;
 
         const insertMovie = await queryDatabase(insertMovie_sql, [
           getMovieResults.title,
           convertedYear,
           getMovieResults.overview,
-          moviePoster,
+          getMovieResults.poster_path,
           getMovieResults.runtime,
+          tmdbId,
         ]);
 
-        const getCast = await fetch(
-          `https://api.themoviedb.org/3/movie/${tmdbId}?language=en-US`,
+        // Sätter in kategorier för en film
+        for (let i = 0; i < getMovieResults.genres.length; i++) {
+          let checkCategory = await queryDatabase(
+            "SELECT categoryId FROM category WHERE categoryName = ?",
+            getMovieResults.genres[i].name,
+          );
+
+          if (checkCategory.length === 0) {
+            let insertNewCategory = await queryDatabase(
+              "INSERT INTO category (categoryName) VALUES (?)",
+              getMovieResults.genres[i].name,
+            );
+            await queryDatabase(
+              "INSERT INTO movieCategory (movieCategoryMID, movieCategoryCID) VALUES (?, ?)",
+              [insertMovie.insertId, insertNewCategory.insertId],
+            );
+          } else {
+            await queryDatabase(
+              "INSERT INTO movieCategory (movieCategoryMID, movieCategoryCID) VALUES (?, ?)",
+              [insertMovie.insertId, checkCategory[0].categoryId],
+            );
+          }
+        }
+
+        // Sätter in cast & crew
+
+        const getCredits = await fetch(
+          `https://api.themoviedb.org/3/movie/${tmdbId}/credits?language=en-US`,
           apiKey,
         );
-        const getCastResults = await getMovieInformation.json();
+        const getCreditsResuls = await getCredits.json();
 
-        res.json(insertMovie.insertId);
+        let insertMovieJobPerson_sql =
+          "INSERT INTO movieJobPerson (movieJobPersonPID, movieJobPersonJID, movieJobPersonMID) VALUES (?, ?, ?)";
+
+        // Importerar skådespelarna
+        for (let i = 0; i < getCreditsResuls.cast.length; i++) {
+          let getPersonInfoF = await fetch(
+            `https://api.themoviedb.org/3/person/${getCreditsResuls.cast[i].id}?language=en-US`,
+            apiKey,
+          );
+          let getPersonInfo = await getPersonInfoF.json();
+          let checkPersonIfExist = await queryDatabase(
+            "SELECT personId FROM person WHERE persontmdbId = ?",
+            getCreditsResuls.cast[i].id,
+          );
+
+          if (checkPersonIfExist.length !== 1) {
+            let insertPerson_sql =
+              "INSERT INTO person (personName, personBorn, personImg, persontmdbId) VALUES (?, ?, ?, ?)";
+            let insertPerson = await queryDatabase(insertPerson_sql, [
+              getPersonInfo.name,
+              getPersonInfo.birthday,
+              getPersonInfo.profile_path,
+              getCreditsResuls.cast[i].id,
+            ]);
+
+            await queryDatabase(insertMovieJobPerson_sql, [
+              insertPerson.insertId,
+              2,
+              insertMovie.insertId,
+            ]);
+          } else {
+            await queryDatabase(insertMovieJobPerson_sql, [
+              checkPersonIfExist[0].personId,
+              2,
+              insertMovie.insertId,
+            ]);
+          }
+        }
+
+        // Importerar crew
+        for (let i = 0; i < getCreditsResuls.crew.length; i++) {
+          let getPersonInfoF = await fetch(
+            `https://api.themoviedb.org/3/person/${getCreditsResuls.crew[i].id}?language=en-US`,
+            apiKey,
+          );
+          let getPersonInfo = await getPersonInfoF.json();
+
+          let checkIfJobExist = await queryDatabase(
+            "SELECT jobId FROM job WHERE jobTitle = ?",
+            getCreditsResuls.crew[i].job,
+          );
+
+          let currentJobId;
+
+          if (checkIfJobExist.length === 0) {
+            let insertNewJob = await queryDatabase(
+              "INSERT INTO job (jobTitle) VALUES (?)",
+              getCreditsResuls.crew[i].job,
+            );
+            currentJobId = insertNewJob.insertId;
+          } else {
+            currentJobId = checkIfJobExist[0].jobId;
+          }
+
+          let checkPersonIfExist = await queryDatabase(
+            "SELECT personId FROM person WHERE persontmdbId = ?",
+            getCreditsResuls.crew[i].id,
+          );
+          if (checkPersonIfExist.length !== 1) {
+            let insertPerson_sql =
+              "INSERT INTO person (personName, personBorn, personImg, persontmdbId) VALUES (?, ?, ?, ?)";
+            let insertPerson = await queryDatabase(insertPerson_sql, [
+              getPersonInfo.name,
+              getPersonInfo.birthday,
+              getPersonInfo.profile_path,
+              getCreditsResuls.crew[i].id,
+            ]);
+
+            await queryDatabase(insertMovieJobPerson_sql, [
+              insertPerson.insertId,
+              currentJobId,
+              insertMovie.insertId,
+            ]);
+          } else {
+            await queryDatabase(insertMovieJobPerson_sql, [
+              checkPersonIfExist[0].personId,
+              currentJobId,
+              insertMovie.insertId,
+            ]);
+          }
+        }
+
+        // res.json(insertMovie.insertId);
       } else {
         res.json({
           error: "Inget resultat...",
@@ -138,6 +315,33 @@ exports.importMovie = async (req, res) => {
   } catch (err) {
     return res.status(500).json({
       error: err.message,
+    });
+  }
+};
+
+// delete movie by id
+exports.deleteMovie = async (req, res) => {
+  const { id } = req.params;
+  let sql = "DELETE FROM movie WHERE movieId = ?";
+
+  if (!id) {
+    return res.status(400).json({
+      success: false,
+      error: "Du har inte skrivit in något ID för filmen du vill radera!",
+    });
+  }
+  try {
+    const deleteMovie = await queryDatabase(sql, id);
+
+    return res.status(201).json({
+      success: true,
+      error: "",
+      message: "Filmen är nu raderad!",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
     });
   }
 };
--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -5,7 +5,17 @@ const movieController = require("../controllers/movieController");
 //router.post('/api/categories', categoryController.createCategory);
 router.get("/api/categories", movieController.getCategories);
 router.get("/api/category/:id", movieController.getCategory);
-router.get("/api/category/:search", movieController.getCategoryBySearch);
+router.get("/api/category/search/:search", movieController.getCategoryBySearch);
 router.get("/api/movies/:amount", movieController.getMovies);
+router.get(
+  "/api/movies/search/:keyword/:amount",
+  movieController.getMovieBySearch,
+);
+router.get("/api/actor/:actor", movieController.getActor);
 router.get("/api/moviesactor/:movie", movieController.getActorsMovie);
+router.get("/api/searcher/:keyword", movieController.getFromSearch);
+
+router.get("/api/importMovie/:tmdbId", movieController.importMovie);
+router.delete("/api/delete/movie/:id", movieController.deleteMovie);
+
 module.exports = router;
