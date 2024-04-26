46c3ec584675a5919f5a543c35be79ba1773a126
Cecilia Rissanen <cecilia.rissanen@iths.com>
2024-04-26 09:15:28 +0200

    en början

--- frontend/src/views/MoviePageView.vue
+++ frontend/src/views/MoviePageView.vue
@@ -1,1 +1,25 @@
-<template></template>
+<script setup>
+import { ref } from vue
+
+const movieDataV = ref(null)
+
+const getMovies = async () => {
+  try {
+    const movies = await fetch('http://localhost:3000/api/movies/10');
+
+    const movieData = await movies.json();
+
+    movieDataV.value = movieData;
+  } catch (e) {
+    console.log(".a.")
+  } finally {
+  }
+}
+
+</script>
+
+<template>
+
+
+
+</template>
