544c1592b7999e7dfde9107936f48b1e79a3d605
Cecilia Rissanen <cecilia.rissanen@iths.com>
2024-04-29 12:56:13 +0200

    moviePage klar förutom link

--- frontend/src/views/MoviePageView.vue
+++ frontend/src/views/MoviePageView.vue
@@ -3,19 +3,19 @@ import { ref } from "vue"
 import { useRoute } from "vue-router"
 
 const router = useRoute()
-
 const movieId = router.params.movieId
-
 const movieData1 = ref(null)
 
 const getMovieData = async () => {
   try {
     const movies = await fetch(`http://localhost:3000/api/specificMovie/${movieId}`);
 
-    const movieData1 = await movies.json();
+    const movieData = await movies.json();
 
     movieData1.value = movieData[0];
-    console.log(movieData)
+
+    console.log(personId)
+    
   } catch (e) {
   } 
 }
@@ -29,10 +29,17 @@ getMovieData()
 <h1> {{ movieData1.movieName }} </h1>
 <img :src="'https://image.tmdb.org/t/p/w500' + movieData1.moviePoster"> 
 <Router-link to="/"><b-button variant="outline-dark">Back</b-button></Router-link>
-<p> {{ movieData1.movieName }} </p>
+<hr>
+<p> Year : {{ movieData1.movieYear }} </p>
+<p> Runtime : {{ movieData1.movieRuntime }} </p>
+<hr>
+<h6> Categories : </h6>
+<li v-for="category in movieData1.categories"> {{ category }} </li>
+<hr>
+<h5>Cast :</h5>
+<li v-for="cast in movieData1.cast"> {{ cast.jobTitle }} : {{ cast.personName }}</li>
 </main>
 </template>
-
 
 <style scoped>
 
@@ -47,5 +54,7 @@ align-items: center;
 img {
   max-width: 350px;  
 }
+
+
 
 </style>
