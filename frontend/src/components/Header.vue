7482b2469a555f4f82bdc715e3879e7c914328a4
Adam Fischer <38475224+f1schhh@users.noreply.github.com>
2024-05-02 11:53:33 +0200

    Fixed search, startpage and movie import on frontend

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,7 +1,102 @@
 <script setup>
+import { ref, computed } from 'vue';
+import { useRouter } from 'vue-router';
+
+const searchInput = ref(null);
+const tmdbId = ref(null);
+const importMsg = ref(null);
+const emoji = ref(null);
+const router = useRouter();
+const isLoading = ref(false);
+const isDisabled = computed(() => {
+  return !searchInput.value
+});
+
+
+function search() {
+  if (searchInput.value) {
+    router.push('/search/all/' + searchInput.value);
+    searchInput.value = ''
+  }
+}
+
+const generateEmoji = () => {
+  const emojis = [
+    '😀', '😃', '😄', '😁', '😆', '😅', '😂', '🤣', '😊', '😇',
+    '🙂', '🙃', '😉', '😌', '😍', '🥰', '😘', '😗', '😙', '😚',
+    '😋', '😛', '😜', '🤪', '😝', '🤑', '🤗', '🤭', '🤫', '🤔',
+    '🤐', '🤨', '😐', '😑', '😶', '😏', '😒', '🙄', '😬', '🤥',
+    '😌', '😔', '😪', '🤤', '😴', '😷', '🤒', '🤕', '🤢', '🤮',
+    '🤧', '🥵', '🥶', '🥴', '😵', '🤯', '🤠', '🥳', '😎', '🤓',
+    '🧐', '😕', '😟', '🙁', '😮', '😯', '😲', '😳', '🥺', '😦',
+    '😧', '😨', '😰', '😥', '😢', '😭', '😱', '😖', '😣', '😞',
+    '😓', '😩', '😫', '🥱', '😤', '😡', '😠', '🤬', '😈', '👿',
+    '💀', '☠️', '💩', '🤡', '👹', '👺', '👻', '👽', '👾', '🤖',
+    '😺', '😸', '😹', '😻', '😼', '😽', '🙀', '😿', '😾', '🙈',
+    '🙉', '🙊', '💋', '💌', '💘', '💝', '💖', '💗', '💓', '💞',
+    '💕', '💟', '❣️', '💔', '❤️', '🧡', '💛', '💚', '💙', '💜',
+    '🤎', '🖤', '🤍', '💯', '💢', '💥', '💫', '💦', '💨', '🕳️',
+    '💣', '💬', '👁️‍🗨️', '🗨️', '🗯️', '💭', '💤', '👋', '🤚', '🖐️',
+    '✋', '🖖', '👌', '🤌', '🤏', '✌️', '🤞', '🤟', '🤘', '🤙',
+    '👈', '👉', '👆', '🖕', '👇', '☝️', '👍', '👎', '✊', '👊',
+    '🤛', '🤜', '👏', '🙌', '👐', '🤲', '🤝', '🙏', '✍️', '💅',
+    '🤳', '💪', '🦾', '🦵', '🦿', '🦶', '👣', '👂', '🦻', '👃',
+    '🫀', '🫁', '🧠', '🦷', '🦴', '👀', '👁️', '👅', '👄', '💋',
+    '🩸', '🩹', '🩺', '🪑', '🪒', '🦽', '🦼', '🛴', '🛹', '🪄',
+    '🪅', '🛷', '🚀', '🛸', '🛎️', '🧳', '⌛', '⏳', '⌚', '⏰',
+    '⏱️', '⏲️', '🕰️', '🌡️', '🔍', '🔎', '🔓', '🔒', '🔏', '🔐',
+    '🔑', '🗝️', '🔨', '⛏️', '⚒️', '🛠️', '🗡️', '⚔️', '🔫', '🏹',
+    '🛡️', '🔧', '🔩', '⚙️', '🗜️', '⚖️', '🦯', '🔗', '⛓️', '🧰',
+    '🛠️', '⛏️', '🔩', '⚙️', '🧲', '🔫', '💣', '🧨', '🪓', '🔪',
+    '🗡️', '🔱', '🏺', '🗿', '🛏️', '🛋️', '🪑', '🚪', '🛌', '🛏️',
+    '🧴', '🚿', '🪒', '🧼', '🪓', '🛁', '🛀', '🪒', '🧻', '🚽',
+    '🚾', '🧻', '🧼', '🧽', '🧴', '🪒', '🧹', '🧺', '🧻', '🚽',
+    '🚿', '🛁', '🛀', '🛋️', '🚪', '🛏️', '🛌', '🧯', '🔥', '🪵',
+    '🪓', '🛢️', '💊', '💉', '🩸', '🦠', '🔬', '🔭', '🧫', '🧪',
+    '🧬', '🧴', '🧹', '🧼', '🪒', '🧽', '🧺', '🧻', '🧯', '🛒',
+    '🚬', '⚰️', '⚱️', '🗿', '🪦', '🚿', '🚽', '🚰', '🚮', '🚻',
+    '🚹', '🚺', '🚼', '🚭', '🅿️', '♿', '🚇', '🚆', '🚈', '🚉',
+    '🚊', '🚝', '🚞', '🚋', '🚌', '🚍', '🚎', '🚐', '🚑', '🚒',
+    '🚓', '🚔', '🚕', '🚖', '🚗', '🚘', '🚙', '🚚', '🚛', '🚜',
+    '🏎️', '🏍️', '🛵', '🦽', '🦼', '🛺', '🚲', '🛴', '🚏', '🛣️',
+    '🛤️', '🛢️', '⛽', '🚨', '🚥', '🚦', '🛑', '🚧', '⚓', '⛵',
+    '🚤', '🛳️', '⛴️', '🛥️', '🚢', '✈️', '🛩️', '🛫', '🛬', '💺',
+    '🚁', '🚟', '🚠', '🚡', '🛰️', '🚀', '🛸', '🌍', '🌎', '🌏',
+    '🌐', '🗺️', '🗾', '🧭', '🏔️', '⛰️', '🌋', '🗻', '🏕️', '🏖️',
+    '🏜️', '🏝️', '🏞️', '🏟️', '🏛️', '🏗️', '🧱', '🪨', '🪵', '🛖',
+    '🏘️', '🏚️', '🏠', '🏡', '🏢', '🏣', '🏤', '🏥', '🏦', '🏨',
+    '🏩', '🏪', '🏫', '🏬', '🏭', '🏯', '🏰', '💒', '🗼', '🗽',
+    '⛪', '🕌', '🛕', '🕍', '⛩️', '🕋', '⛲', '⛺', '🌁', '🌃',
+    '🏙️', '🌄', '🌅', '🌆', '🌇', '🌉', '🎠', '🎡', '🎢', '💈',
+    '🎪', '🚂', '🚃', '🚄', '🚅', '🚆', '🚇', '🚈', '🚉', '🚊',
+    '🚝', '🚞', '🚋', '🚌', '🚍', '🚎', '🚐', '🚑', '🚒', '🚓',
+    '🚔', '🚕', '🚖', '🚗', '🚘', '🚙', '🚚', '🚛', '🚜',];
+  const randomIndex = Math.floor(Math.random() * emojis.length);
+  emoji.value = emojis[randomIndex];
+};
+
+async function importMovie() {
+  try {
+    isLoading.value = true;
+
+    setInterval(generateEmoji, 500);
+
+    const importMovie = await fetch(`http://localhost:3000/api/importMovie/${tmdbId.value}`);
+    const response = await importMovie.json();
+    console.log(response, "KING")
+    if (response.success) {
+      tmdbId.value = ''
+      importMsg.value = 'ALLT GICK BRA !!'
+    }
+  } catch (e) {
+    importMsg.value = e;
+  } finally {
+    isLoading.value = false;
+  }
+}
+
 
 </script>
-
 <template>
   <b-container fluid="lg" class="mt-2 mb-3">
     <b-navbar toggleable="lg" type="white" variant="white">
@@ -19,10 +114,35 @@
             <b-button variant="primary" class="my-2 my-sm-0" type="submit" @click="search()"
               :disabled="isDisabled">Sök</b-button>
           </b-nav-form>
+          <b-button variant="primary" class="my-2 my-sm-0" type="button" v-b-modal.modal-1>✏️</b-button>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
   </b-container>
+  <b-modal id="modal-1" title="Importera en film" hide-footer>
+    <div class="d-flex flex-column align-items-center gap-2 text-center" v-if="isLoading">
+      <b-spinner label="Loading..."></b-spinner>
+      <span class="fs-5">Importerar film {{ emoji }}</span>
+    </div>
+    <b-row cols="1" class="d-flex flex-column gap-3" v-else>
+      <b-col class="d-flex flex-column" v-if="importMsg">
+        {{ importMsg }}
+      </b-col>
+      <b-col class="d-flex flex-column">
+        <b-form-input class="mr-sm-1 mb-3" placeholder="The movie database ID behövs 🎭 🎟️" v-model="tmdbId"
+          required></b-form-input>
+      </b-col>
+      <b-col class="d-flex flex-column">
+        <div>
+          Id:et hittar du <a href="https://themoviedb.org" target="_blank" class="mt-5">HÄR</a>
+        </div>
+      </b-col>
+      <b-col class="d-flex flex-column">
+        <b-button variant="primary" class="my-2 my-sm-0" type="button" @click="importMovie()">Importera 🧑‍🎓</b-button>
+      </b-col>
+
+    </b-row>
+  </b-modal>
 </template>
 
 <style scoped></style>
--- frontend/src/views/SearchPageView.vue
+++ frontend/src/views/SearchPageView.vue
@@ -1,1 +1,73 @@
-<template></template>
+<script setup>
+import { ref, watch } from 'vue';
+import { useRoute, useRouter } from "vue-router"
+
+const route = useRoute()
+const router = useRouter()
+
+const keyword = ref(route.params.keyword);
+const SearchArr = ref(null);
+
+async function getSearchResults() {
+  try {
+    const getSearch = await fetch(`http://localhost:3000/api/searcher/${keyword.value}`);
+    const getAllResults = await getSearch.json();
+    console.log(getAllResults)
+    SearchArr.value = getAllResults[0];
+  } catch {
+
+  }
+}
+
+const goToURL = (id, type) => {
+  router.push(`/${type}/${id}`)
+}
+
+watch(
+  () => route.params.keyword,
+  (newKeyword, oldKetword) => {
+    keyword.value = newKeyword;
+    getSearchResults();
+  }
+);
+
+getSearchResults();
+</script>
+
+<template>
+  <b-container fluid="lg" class="d-flex flex-column mt-2 gap-4">
+    <h4 class="mb-6">Sökresultat för {{ keyword }} 🎬 📹🎥 📽️</h4>
+    <b-row v-if="SearchArr" cols="2" cols-sm="3" cols-md="4" cols-lg="6">
+      <b-col class="d-flex flex-column" v-for="searchArr in SearchArr">
+        <div @click="goToURL(searchArr.id, searchArr.type)" class="movie-container">
+          <div class="img-wrapper">
+            <img :src="'https://image.tmdb.org/t/p/w300' + searchArr.poster" alt="" class="img-fluid"
+              v-if="searchArr.poster">
+            <img src="https://placehold.co/200x300/212529/FFF?text=?" alt="" class="img-fluid" v-else>
+          </div>
+          <p class="fs">{{ searchArr.name }}</p>
+        </div>
+      </b-col>
+      <div v-if="SearchArr.length === 0">
+        <p>No results found 👺 👺 👺 </p>
+      </div>
+    </b-row>
+    <div class="text-center mb-3" v-else>
+      <b-spinner class="color-text-custom" label="Loading..."></b-spinner>
+    </div>
+  </b-container>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+}
+
+.movie-container:hover {
+  cursor: pointer;
+}
+
+.img-wrapper {
+  max-width: 191px;
+}
+</style>
